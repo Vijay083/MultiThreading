1826bfb8f6b5e79585dceb534b54f3d3b673a377
vakavijay083 <vijaybhaskarreddy@email.saintleo.edu>
2023-12-05 18:46:49 -0500

    Changes made on main

--- MultiThreading/src/main/java/com/telusko/MultiThreading/MultiThreadingApplication.java
+++ MultiThreading/src/main/java/com/telusko/MultiThreading/MultiThreadingApplication.java
@@ -3,11 +3,44 @@ package com.telusko.MultiThreading;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+class A extends Thread{
+	public void run() {
+		for(int i=0;i<100;i++) {
+		System.out.println("Hi");
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		}
+	}
+}
+
+class B extends Thread{
+	public void run() {
+		for(int i=0;i<100;i++) {
+			System.out.println("Hello");
+		
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		}
+	}
+}
 @SpringBootApplication
 public class MultiThreadingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MultiThreadingApplication.class, args);
+		
+		A obj1 = new A();
+		obj1.start();
+		B obj2 = new B();
+		obj2.start();
 	}
 
 }
