0cd3d30e080f32fb002f2c188ac31ac685358072
vakavijay083 <vijaybhaskarreddy@email.saintleo.edu>
2023-12-05 22:03:30 -0500

    branch1

--- MultiThreading/src/main/java/com/telusko/MultiThreading/MultiThreadingApplication.java
+++ MultiThreading/src/main/java/com/telusko/MultiThreading/MultiThreadingApplication.java
@@ -20,7 +20,7 @@ class A extends Thread{
 class B extends Thread{
 	public void run() {
 		for(int i=0;i<100;i++) {
-			System.out.println("Hello Bhaskar");
+			System.out.println("Hello Bhaskar Reddy");
 		
 		try {
 			Thread.sleep(10);
